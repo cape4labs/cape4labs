5ccd2beae1bd7fc7e4b3df172aefa12c972a802c
isgin01 <isgin01@gmail.com>
2025-12-23 01:11:36 +0500

    Delete empty project pages

--- src/app/first-project/page.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "@/src/components/Header"
-
-export default function FirstProject() {
-    return (
-        <div className="flex min-h-screen">
-            <Header />
-        </div>
-    )
-}
