2c369f4d6bc68252a6c644d66b38353a5a350462
qwaqd <qwaq.dev@gmail.com>
2025-12-25 09:18:48 +0500

    Delete unnecessary folders

--- src/app/second-project/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function FirstProject() {
-    return (
-        <div className="flex min-h-screen">
-            <h2>second</h2>
-        </div>
-    )
-}
--- src/app/third-project/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function FirstProject() {
-    return (
-        <div className="flex min-h-screen">
-            <h2>third</h2>
-        </div>
-    )
-}
