bcf53eacd4289b9ab135ad86667d68076de7de4a
isgin01 <isgin01@gmail.com>
2026-01-25 14:40:28 +0500

    Hide text in Contact component behind image

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,14 +5,14 @@ const ASSETS = {
 export default function Contact() {
     return (
         <div className="flex flex-col text-center pb-15">
-            <h2 className="font-primary text-6xl z-10 translate-y-11 -translate-x-9 text-midnight-soft">
+            <h2 className="font-primary text-6xl z-0 translate-y-11 -translate-x-9 text-midnight-soft">
                 CONTACT US
                 <br />
                 TO BUILD YOUR
                 <br />
                 DREAM PROJECT
             </h2>
-            <img src={ASSETS.hand} className="w-full object-contain mb-5 z-0" />
+            <img src={ASSETS.hand} className="w-full object-contain mb-5 z-10" />
             <h2 className="font-primary text-5xl text-midnight-light">cape4labs@proton.me</h2>
         </div>
     )
